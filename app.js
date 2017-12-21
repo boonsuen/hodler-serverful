a80e9cbf84fbc38891ad53455a9bf709b3d0fa20
Boonsuen Oh <ohboonsuen123@gmail.com>
2017-12-21 23:28:33 +0800

    Add Interested page

--- app.js
+++ app.js
@@ -10,87 +10,97 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
+var prices = {};
+
 app.get('/', (req, res) => {
-  var pu_BTC;
-  var pu_MIOTA;
-  var pu_POWR;
-  var pu_ARDR;
-  var pu_RDN;
-  var pu_OMG;
-  var pu_WABI;
-  var pu_PING;
-
-  var ps_MIOTA;
-  var ps_POWR;
-  var ps_ARDR;
-  var ps_RDN;
-  var ps_OMG;
-  var ps_WABI;
-  var ps_PING;
-
   axios.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/').then((response) => {
-    if (response.data.status === 'ZERO_RESULTS') {
-      throw new Error('Unable to fetch data.');
-    }
-
-    pu_BTC = response.data[0].price_usd;
+    prices.pu_BTC = response.data[0].price_usd;
+    return axios.get('https://api.coinmarketcap.com/v1/ticker/litecoin/');
+  }).then((response) => {
+    prices.pu_LTC = response.data[0].price_usd;
+    prices.ps_LTC = response.data[0].price_btc;
     return axios.get('https://api.coinmarketcap.com/v1/ticker/iota/');
   }).then((response) => {
-    pu_MIOTA = response.data[0].price_usd;
-    ps_MIOTA = response.data[0].price_btc;
+    prices.pu_MIOTA = response.data[0].price_usd;
+    prices.ps_MIOTA = response.data[0].price_btc;
     return axios.get('https://api.coinmarketcap.com/v1/ticker/power-ledger/');
   }).then((response) => {
-    pu_POWR = response.data[0].price_usd;
-    ps_POWR = response.data[0].price_btc;
+    prices.pu_POWR = response.data[0].price_usd;
+    prices.ps_POWR = response.data[0].price_btc;
     return axios.get('https://api.coinmarketcap.com/v1/ticker/ardor/');
   }).then((response) => {
-    pu_ARDR = response.data[0].price_usd;
-    ps_ARDR = response.data[0].price_btc;
+    prices.pu_ARDR = response.data[0].price_usd;
+    prices.ps_ARDR = response.data[0].price_btc;
     return axios.get('https://api.coinmarketcap.com/v1/ticker/wabi/');
   }).then((response) => {
-    pu_WABI = response.data[0].price_usd;
-    ps_WABI = response.data[0].price_btc;
-    return axios.get('https://api.coinmarketcap.com/v1/ticker/cryptoping/');
-  }).then((response) => {
-    pu_PING = response.data[0].price_usd;
-    ps_PING = response.data[0].price_btc;
+    prices.pu_WABI = response.data[0].price_usd;
+    prices.ps_WABI = response.data[0].price_btc;
     return axios.get('https://api.coinmarketcap.com/v1/ticker/raiden-network-token/');
   }).then((response) => {
-    pu_RDN = response.data[0].price_usd;
-    ps_RDN = response.data[0].price_btc;
+    prices.pu_RDN = response.data[0].price_usd;
+    prices.ps_RDN = response.data[0].price_btc;
     return axios.get('https://api.coinmarketcap.com/v1/ticker/omisego/');
   }).then((response) => {
-    pu_OMG = response.data[0].price_usd;
-    ps_OMG = response.data[0].price_btc;
+    prices.pu_OMG = response.data[0].price_usd;
+    prices.ps_OMG = response.data[0].price_btc;
+    console.log(prices);
     res.render('home.hbs', {
-      pu_BTC,
-      pu_MIOTA: Number(Math.round(pu_MIOTA+'e2')+'e-2'),
-      pu_POWR: Number(Math.round(pu_POWR+'e2')+'e-2'),
-      pu_ARDR: Number(Math.round(pu_ARDR+'e2')+'e-2'),
-      pu_RDN: Number(Math.round(pu_RDN+'e2')+'e-2'),
-      pu_OMG: Number(Math.round(pu_OMG+'e2')+'e-2'),
-      pu_WABI: Number(Math.round(pu_WABI+'e2')+'e-2'),
-      pu_PING: Number(Math.round(pu_PING+'e2')+'e-2'),
-      ps_MIOTA: Math.floor(ps_MIOTA * 100000000),
-      ps_POWR: Math.floor(ps_POWR * 100000000),
-      ps_ARDR: Math.floor(ps_ARDR * 100000000),
-      ps_RDN: Math.floor(ps_RDN * 100000000),
-      ps_OMG: Math.floor(ps_OMG * 100000000),
-      ps_WABI: Math.floor(ps_WABI * 100000000),
-      ps_PING: Math.floor(ps_PING * 100000000)
+      hold: "active",
+      interest: "not-active",
+      pu_BTC: Number(Math.round(prices.pu_BTC+'e2')+'e-2'),
+      pu_LTC: Number(Math.round(prices.pu_LTC+'e2')+'e-2'),
+      pu_MIOTA: Number(Math.round(prices.pu_MIOTA+'e2')+'e-2'),
+      pu_POWR: Number(Math.round(prices.pu_POWR+'e2')+'e-2'),
+      pu_ARDR: Number(Math.round(prices.pu_ARDR+'e2')+'e-2'),
+      pu_RDN: Number(Math.round(prices.pu_RDN+'e2')+'e-2'),
+      pu_OMG: Number(Math.round(prices.pu_OMG+'e2')+'e-2'),
+      pu_WABI: Number(Math.round(prices.pu_WABI+'e2')+'e-2'),
+      ps_LTC: Math.floor(prices.ps_LTC * 100000000),
+      ps_MIOTA: Math.floor(prices.ps_MIOTA * 100000000),
+      ps_POWR: Math.floor(prices.ps_POWR * 100000000),
+      ps_ARDR: Math.floor(prices.ps_ARDR * 100000000),
+      ps_RDN: Math.floor(prices.ps_RDN * 100000000),
+      ps_OMG: Math.floor(prices.ps_OMG * 100000000),
+      ps_WABI: Math.floor(prices.ps_WABI * 100000000)
     });
   }).catch((e) => {
     if (e.code === 'ENOTFOUND') {
-      console.log('Unable to connect to API servers.');
+      res.send('Unable to connect to API servers.');
     } else {
-      console.log(e.message);
+      res.send(e.message);
     }
   });
 });
 
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: "Error!"
+app.get('/interested', (req, res) => {
+  axios.get('https://api.coinmarketcap.com/v1/ticker/ethereum/').then((response) => {
+    prices.pu_ETH = response.data[0].price_usd;
+    prices.ps_ETH = response.data[0].price_btc;
+    return axios.get('https://api.coinmarketcap.com/v1/ticker/hempcoin/');
+  }).then((response) => {
+    prices.pu_THC = response.data[0].price_usd;
+    prices.ps_THC = response.data[0].price_btc;
+    return axios.get('https://api.coinmarketcap.com/v1/ticker/monaco/');
+  }).then((response) => {
+    prices.pu_MCO = response.data[0].price_usd;
+    prices.ps_MCO = response.data[0].price_btc;
+    console.log(prices);
+    res.render('interested.hbs', {
+      hold: "not-active",
+      interest: "active",
+      pu_ETH: Number(Math.round(prices.pu_ETH+'e2')+'e-2'),
+      pu_THC: Number(Math.round(prices.pu_THC+'e2')+'e-2'),
+      pu_MCO: Number(Math.round(prices.pu_MCO+'e2')+'e-2'),
+      ps_ETH: Math.floor(prices.ps_ETH * 100000000),
+      ps_THC: Math.floor(prices.ps_THC * 100000000),
+      ps_MCO: Math.floor(prices.ps_MCO * 100000000)
+    });
+  }).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+      res.send('Unable to connect to API servers.');
+    } else {
+      res.send(e.message);
+    }
   });
 });
 
