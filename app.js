1626b182e93c62bc6089a37c7736d81b94daf9be
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-02-21 18:29:06 +0800

    app.js: code refactor

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const hbs = require('hbs');
 const port = process.env.PORT || 3000;
 const app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials('views/partials');
 app.set('view engine', 'hbs');
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 const mainRoutes = require('./routes');
 const liteRoutes = require('./routes/lite.js');
